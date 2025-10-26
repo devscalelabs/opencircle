c0f07a9ec3fba963730ccad3b1622719f4a4ea08
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-10-26 13:01:46 +0700

    feat: adding social links

--- .releaserc.js
+++ .releaserc.js
@@ -3,14 +3,6 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["semantic-release-monorepo", {
-      "packages": [
-        "package.json",
-        "packages/*/package.json",
-        "apps/*/package.json",
-        "apps/api/pyproject.toml"
-      ]
-    }],
     ["@semantic-release/changelog", {
       "changelogFile": "CHANGELOG.md"
     }],
@@ -19,7 +11,8 @@ module.exports = {
       "tarballDir": "dist"
     }],
     ["@semantic-release/git", {
-      "assets": ["CHANGELOG.md", "package.json", "packages/*/package.json", "apps/*/package.json", "apps/api/pyproject.toml"],
+      "assets": ["CHANGELOG.md", "package.json", "packages/*/package.json", "apps/*/package.json",
+"apps/api/pyproject.toml"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }]
   ]
