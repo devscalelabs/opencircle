d9cee673d93206cc3b0e73bd813c5e93c0c3e9d9
indrazm <me@indrazm.com>
2025-11-30 18:44:33 +0700

    adding protection to resources

--- apps/platform/src/features/resources/hooks/useResources.ts
+++ apps/platform/src/features/resources/hooks/useResources.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
+import { HTTPError } from "ky";
 import { api } from "../../../utils/api";
 
 export const useResources = (channelId?: string) => {
-	const { data, isLoading } = useQuery({
+	const { data, isLoading, error } = useQuery({
 		queryKey: ["resources", { channelId }],
 		queryFn: async () => {
 			if (channelId) {
@@ -10,10 +11,18 @@ export const useResources = (channelId?: string) => {
 			}
 			return await api.resources.getAll();
 		},
+		retry: (failureCount, error) => {
+			if (error instanceof HTTPError && error.response.status === 403) {
+				console.error("Not Eligible to access");
+				return false;
+			}
+			return failureCount < 3;
+		},
 	});
 
 	return {
 		resources: data,
 		isResourcesLoading: isLoading,
+		error,
 	};
 };
